3932add49ee9dfb71783e15aab5ebd50bc375818
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-10-21 16:27:46 +0100

    chat support

--- app.py
+++ app.py
@@ -2,14 +2,32 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, abort
 from twilio.jwt.access_token import AccessToken
-from twilio.jwt.access_token.grants import VideoGrant
+from twilio.jwt.access_token.grants import VideoGrant, IpMessagingGrant
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioException, TwilioRestException
 
 load_dotenv()
 twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
 twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
+try:
+    twilio_client = Client()
+except TwilioException:
+    twilio_client = None
 
 app = Flask(__name__)
+
+
+def get_chatroom(name):
+    if twilio_client is None:
+        return
+    for conversation in twilio_client.conversations.conversations.list():
+        if conversation.friendly_name == name:
+            return conversation
+
+    # a conversation with the given name does not exist ==> create a new one
+    return twilio_client.conversations.conversations.create(
+        friendly_name=name)
 
 
 @app.route('/')
@@ -23,11 +41,26 @@ def login():
     if not username:
         abort(401)
 
+    conversation = get_chatroom(os.environ.get('CHATROOM', 'My Room'))
+    conversation_sid = ''
+    if conversation:
+        try:
+            conversation.participants.create(identity=username)
+        except TwilioRestException as ex:
+            # do not error if the user is already in the conversation
+            if ex.status != 409:
+                raise
+        conversation_sid = conversation.sid
+
     token = AccessToken(twilio_account_sid, twilio_api_key_sid,
                         twilio_api_key_secret, identity=username)
     token.add_grant(VideoGrant(room='My Room'))
+    if conversation:
+        token.add_grant(IpMessagingGrant(
+            service_sid=conversation.chat_service_sid))
 
-    return {'token': token.to_jwt().decode()}
+    return {'token': token.to_jwt().decode(),
+            'conversation_sid': conversation_sid}
 
 
 if __name__ == '__main__':
